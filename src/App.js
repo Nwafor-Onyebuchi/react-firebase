2f2b8927c0e556bb2ba01cdf1fa270ee0d387b35
buchi <buchilazarus4@gmail.com>
2020-09-30 23:08:07 +0100

    complete auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Header from "./Header";
 import "./App.css";
 import "./firebase/config";
-import SignUp from "./pages/SignUp";
+import "./pages/SignUp";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Signup from "./pages/SignUp";
+import Login from "./pages/Login";
 import { UserProvider } from "./firebase/UserProvider";
 import Profile from "./pages/Proflie";
 
@@ -10,12 +13,13 @@ function App() {
   return (
     <UserProvider>
       <BrowserRouter>
-        <div className="App">
-          <header className="App-header"></header>
-          <div className="container">
+        <Header></Header>
+        <div className="app">
+          <div className="ui grid container">
             <Switch>
-              <Route exact path="/signup" component={SignUp} />
-              <Route exact path="/profile" component={Profile} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:id" component={Profile} />
+              <Route exact path="/login" component={Login} />
             </Switch>
           </div>
         </div>
